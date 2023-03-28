bb1f8aed4e19e4bd7002d15c9fcf1761a3db979a
Soha Kamal <alskafy@gmail.com>
2023-03-28 22:14:54 +0300

    first commit

---
+++ calculator.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Calculator</title>
+</head>
+<body>
+  <h1>Calculator</h1>
+  <form action ="calculator.php" method="get"
+    <input type="number" name="num1"
+
+
+  < input type="submit">
+</form>
+
+<?php 
+
+?>
